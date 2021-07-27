5d246f8ff03411df37f093759d9abfab094a41b8
Steve Araya Solórzano <stevenaraya.24@gmail.com>
2021-07-26 18:25:32 -0600

    Trabajando en el servicio de python

---
+++ microservicios/service-multiplicacion/app.py
@@ -0,0 +1,11 @@
+
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/multiplicacion/<numero1>/por/<numero2>')
+def multiplicar(numero1, numero2):
+    result = int(numero1) * int(numero2)
+    return str(result)
